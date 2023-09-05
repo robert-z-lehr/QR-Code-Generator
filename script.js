2d1399056f0a8f1b19d3f91e447dd16201a8665e
RobertZygmuntLehr <robert.z.lehr@gmail.com>
2023-09-05 18:48:12 -0500

    Update .html, .js, and .css files

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
+// Generate QR Code
 function generateQRCode() {
-    if (window.innerWidth <= 600) {
-        document.body.style.fontSize = "18px";
-      }      
-    
-    // Clear previous QR Code
-    document.getElementById("qrcode").innerHTML = "";
-  
-    // Get the URL
-    const url = document.getElementById("url-input").value;
-  
-    // Generate new QR Code
-    new QRCode(document.getElementById("qrcode"), {
-      text: url,
-      width: 512,
-      height: 512,
-      colorDark: "#000000",
-      colorLight: "#ffffff",
-      correctLevel: QRCode.CorrectLevel.L
-    });
-  }
-  
+  // Clear previous QR Code
+  document.getElementById("qrcode").innerHTML = "";
+
+  // Get the URL
+  const url = document.getElementById("url-input").value;
+
+  // Generate new QR Code
+  new QRCode(document.getElementById("qrcode"), {
+    text: url,
+    width: 256,
+    height: 256,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+    correctLevel: QRCode.CorrectLevel.L
+  });
+}
