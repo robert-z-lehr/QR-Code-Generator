a523a1a4aeec6bd2ee59ed806e4fc0d660452df9
RobertZygmuntLehr <robert.z.lehr@gmail.com>
2023-09-05 19:00:32 -0500

    Update .html, .js, and .css

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-// Generate QR Code
+// Function to generate QR Code
 function generateQRCode() {
-  // Clear previous QR Code
+  // Clear any existing QR Code from the container
   document.getElementById("qrcode").innerHTML = "";
 
-  // Get the URL
+  // Retrieve the URL entered by the user
   const url = document.getElementById("url-input").value;
 
-  // Generate new QR Code
+  // Generate a new QR Code using the library
   new QRCode(document.getElementById("qrcode"), {
-    text: url,
-    width: 200,
-    height: 200,
-    colorDark: "#000000",
-    colorLight: "#ffffff",
-    correctLevel: QRCode.CorrectLevel.L
+    text: url,             // Text to be encoded
+    width: 256,            // Width of the QR Code
+    height: 256,           // Height of the QR Code
+    colorDark: "#000000",  // Dark color for the QR Code
+    colorLight: "#ffffff", // Light color for the QR Code
+    correctLevel: QRCode.CorrectLevel.L // Error correction level
   });
 }
+
+// Event listener for the 'Download QR Code' button
+document.getElementById("downloadQR").addEventListener("click", function() {
+  // Retrieve the canvas element that holds the QR Code
+  const canvas = document.querySelector("#qrcode canvas");
+
+  // Convert the canvas to a downloadable image format (PNG)
+  const imgURL = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+
+  // Create a download link element
+  const link = document.createElement("a");
+
+  // Set the link attributes and trigger the download
+  link.download = 'QRCode.png';
+  link.href = imgURL;
+  link.click();
+});
