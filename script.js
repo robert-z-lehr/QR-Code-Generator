afe64f0818468d474dfee5b036de625fd4bbb460
RobertZygmuntLehr <robert.z.lehr@gmail.com>
2023-09-05 21:36:58 -0500

    Update JS

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
+// // Function to generate QR Code
+// function generateQRCode() {
+//   const qrWrapper = document.getElementById("qrWrapper");
+//   console.log("generateQRCode called"); // Add this line for debugging
+//   qrWrapper.innerHTML = "";
+
+//   const url = document.getElementById("url-input").value;
+
+//   new QRCode(qrWrapper, {
+//     text: url,
+//     width: 256,
+//     height: 256,
+//     colorDark: "#000000",
+//     colorLight: "#ffffff",
+//     correctLevel: QRCode.CorrectLevel.L
+//   });
+// }
+
 // Function to generate QR Code
 function generateQRCode() {
   const qrWrapper = document.getElementById("qrWrapper");
-  console.log("generateQRCode called"); // Add this line for debugging
+  const loadingMessage = document.createElement("p");
+  loadingMessage.textContent = "Generating QR Code..."; // Add a loading message
   qrWrapper.innerHTML = "";
+  qrWrapper.appendChild(loadingMessage); // Add the loading message
 
   const url = document.getElementById("url-input").value;
 
-  new QRCode(qrWrapper, {
-    text: url,
-    width: 256,
-    height: 256,
-    colorDark: "#000000",
-    colorLight: "#ffffff",
-    correctLevel: QRCode.CorrectLevel.L
-  });
+  // Simulate a delay (you can remove this in production)
+  setTimeout(() => {
+    qrWrapper.innerHTML = ""; // Clear the loading message
+    new QRCode(qrWrapper, {
+      text: url,
+      width: 256,
+      height: 256,
+      colorDark: "#000000",
+      colorLight: "#ffffff",
+      correctLevel: QRCode.CorrectLevel.L
+    });
+  }, 1000); // Simulated delay of 1 second (adjust as needed)
 }
 
 // Event listener for the 'Download QR Code' button
