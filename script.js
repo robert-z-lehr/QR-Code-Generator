16aa34c237de7930edf64f61470c7faa28011ccd
RobertZygmuntLehr <robert.z.lehr@gmail.com>
2023-09-05 21:33:50 -0500

    Update JS

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-// // Function to generate QR Code
-// function generateQRCode() {
-//   const qrWrapper = document.getElementById("qrWrapper");
-//   qrWrapper.innerHTML = "";
-
-//   const url = document.getElementById("url-input").value;
-
-//   new QRCode(qrWrapper, {
-//     text: url,
-//     width: 256,
-//     height: 256,
-//     colorDark: "#000000",
-//     colorLight: "#ffffff",
-//     correctLevel: QRCode.CorrectLevel.L
-//   });
-// }
-
-// // Event listener for the 'Download QR Code' button
-// document.getElementById("downloadQR").addEventListener("click", function() {
-//   const canvas = document.querySelector("#qrWrapper canvas");
-
-//   const imgURL = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
-
-//   const link = document.createElement("a");
-//   link.download = 'QRCode.png';
-//   link.href = imgURL;
-//   link.click();
-// });
-
 // Function to generate QR Code
 function generateQRCode() {
   const qrWrapper = document.getElementById("qrWrapper");
