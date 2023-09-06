d88b4c0488042368246d29bfa1b31b1fc21a1bb9
RobertZygmuntLehr <robert.z.lehr@gmail.com>
2023-09-05 20:53:03 -0500

    Upate HTML, JS

--- script.js
+++ script.js
@@ -1,38 +1,28 @@
-// Wrap your code in window.onload
-window.onload = function () {
-  // Function to generate QR Code
-  function generateQRCode() {
-    const qrWrapper = document.getElementById("qrWrapper");
-    qrWrapper.innerHTML = "";
+// Function to generate QR Code
+function generateQRCode() {
+  const qrWrapper = document.getElementById("qrWrapper");
+  qrWrapper.innerHTML = "";
 
-    const url = document.getElementById("url-input").value;
+  const url = document.getElementById("url-input").value;
 
-    new QRCode(qrWrapper, {
-      text: url,
-      width: 256,
-      height: 256,
-      colorDark: "#000000",
-      colorLight: "#ffffff",
-      correctLevel: QRCode.CorrectLevel.L
-    });
-  }
-
-  // Event listener for the 'Generate QR Code' button
-  document.getElementById("generate-button").addEventListener("click", generateQRCode);
-
-  // Event listener for the 'Download QR Code' button
-  document.getElementById("downloadQR").addEventListener("click", function() {
-    const canvas = document.querySelector("#qrWrapper canvas");
-
-    const imgURL = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
-
-    const link = document.createElement("a");
-    link.download = 'QRCode.png';
-    link.href = imgURL;
-    link.click();
+  new QRCode(qrWrapper, {
+    text: url,
+    width: 256,
+    height: 256,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+    correctLevel: QRCode.CorrectLevel.L
   });
-
-  // Remove the "Wait please, resources are being loaded" message when the page is loaded
-  const loadingMessage = document.getElementById("loading-message");
-  loadingMessage.style.display = "none";
 }
+
+// Event listener for the 'Download QR Code' button
+document.getElementById("downloadQR").addEventListener("click", function() {
+  const canvas = document.querySelector("#qrWrapper canvas");
+
+  const imgURL = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+
+  const link = document.createElement("a");
+  link.download = 'QRCode.png';
+  link.href = imgURL;
+  link.click();
+});
